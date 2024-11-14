2f7c6ae75139bcad7d0ac6a61ae8a7b53206c25d
marnap6 <148319540+marnap6@users.noreply.github.com>
2024-11-14 23:57:36 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "readFile.h"
+using namespace std;
+
+int main() {
+    readFile read;
+    read.readFasta("C:\\Users\\marci\\CLionProjects\\zajecia_obiekt\\fasta.txt");
+    return 0;
+}
---
+++ readFile.h
@@ -0,0 +1,16 @@
+//
+// Created by marci on 04.10.2024.
+//
+
+#ifndef UNTITLED4_READFILE_H
+#define UNTITLED4_READFILE_H
+
+#include <string>
+
+class readFile {
+public:
+    void readFasta(const std::string& name_file);
+};
+
+
+#endif //UNTITLED4_READFILE_H
